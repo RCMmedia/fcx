e8b0b9bbd555d2c86d4e975f5ce75dbb93cba236
Jason Monsef <jasonmonsef@gmail.com>
2016-06-03 11:02:50 -0700

    changes

--- wp-content/themes/fcx/footer.php
+++ wp-content/themes/fcx/footer.php
@@ -1,7 +1,26 @@
 <div class="footer">
 	Franchise Capital Exchange is a production of O’Connell & Company, Inc. / FranWise®
 </div>
-</div><!-- .content -->
+
+</div><!-- .content opened on header.php -->
+
+<div class="overlay_wrap">
+	<div class="overlay_inner">
+
+		<?php if( have_rows('home_page_links', 8) ): ?>
+		   <?php while ( have_rows('home_page_links', 8) ) : the_row(); ?>
+		
+		      <div class="page_link">
+			      <a href="<?php the_sub_field('page_link_url', 8 ); ?>">
+				      <span><?php the_sub_field('home_page_link_text'); ?></span>
+			      </a>
+		      </div>
+		
+		    <?php endwhile; ?>
+		<?php endif; ?>
+		
+	</div>
+</div>
 
 <script>
 	$(document).ready(function(){
@@ -14,30 +33,64 @@
 		$(this).find('span.button').fadeOut();
 	});
 	
-});
+	
+	
+}); // end document ready
+
+//overlay functions
+$(document).on("click", ".trigger-overlay", function () {
+	$('.overlay_wrap').toggleClass('active');
+}); 
+	
+/*
 // do something after scrolling past header
-var bottom = $('.trigger_nav').offset().top;
+var bottom = $('.header-wrap').offset().top;
 $(window).scroll(function(){    
-    if ($(this).scrollTop() > bottom){ 
-        $('.nav-wrap').addClass('fixed'); 
+	if ($(this).scrollTop() > bottom){ 
+		$('.nav-wrap').addClass('fixed'); 
+	}
+	else{
+		$('.nav-wrap').removeClass('fixed');
+	}
+});
+*/
+
+
+
+
+//responsive JS with modernizer
+var mod = function(){
+		if (Modernizr.mq('(max-width: 700px)')) {
+	  	$( "#sidebar" ).insertAfter( $( ".header-wrap" ) );
+	        
+	  } else {
+	  	// Clear the settings etc
+	  	$( "#sidebar" ).insertBefore( $( "#content" ) );
+	  	
+	  	var topofDiv = $(".header").offset().top; //gets offset of header
+var height = $(".header").outerHeight(); //gets height of header
+
+$(window).scroll(function(){
+    if($(window).scrollTop() > (topofDiv + height)){
+       $('.nav-wrap').addClass('fixed');
     }
     else{
-        $('.nav-wrap').removeClass('fixed');
+       $('.nav-wrap').removeClass('fixed');
     }
 });
-//responsive JS
-var media = matchMedia('(max-width: 700px)');
-media.addListener(function (mediaQueryList) {
-if (mediaQueryList.matches) {
-//$('#sidebar').hide()
-$( "#sidebar" ).insertAfter( $( ".header-wrap" ) );
-} else {
-//$('#sidebar').show()
-$( "#sidebar" ).insertAfter( $( "body" ) );
-
-}
-});
+	  }
+	}
+	
+	// Shortcut for $(document).ready()
+	$(function() {
+		// Call on every window resize
+	  $(window).resize(mod);
+	  // Call once on initial load
+	  mod();
+	});
 </script>
+
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/fcx/header.php
+++ wp-content/themes/fcx/header.php
@@ -2,11 +2,15 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1" />
 <title><?php  wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> 
+<script src="<?php bloginfo('template_url') ?>/js/modernizr.js"></script> 
+<script src="<?php bloginfo('template_url') ?>/js/headhesive.min.js"></script> 
+
 <style>
 	<?php if( get_field('banner_image')) { ?>
 		.header {
@@ -22,6 +26,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+	
 <div id="sidebar">
 	<h2 class="event-date">August 24-26, 2016</h2>
 	<span class="event-location">Downtown<br> Chicago, IL</span>
@@ -29,35 +34,39 @@
 	<span class="event-phonenumber"><a href="tel:7144341516">714.434.1516</a></span>
 	<span class="event-email"><a href="mailto:Info@FranchiseCapitalExchange.com">Info@FranchiseCapitalExchange.com</a></span>
 	<a class="button-register button" href="">Register <span style="font-size: 12px; display: inline;">&#9654;</span></a>
-</div>
+</div><!-- #sidebar -->
+
 <div id="content">
+	
 	<div class="nav-wrap">
+		
 		<div class="logo">
 			<a href="<?php bloginfo('url') ?>">
 				<img class="logo-big" src="<?php bloginfo('template_url') ?>/images/logo-sm.png" alt="logo-sm" width="382" height="34">
-				<img class="logo-small" src="<?php bloginfo('template_url') ?>/images/logo-mobile.png" alt="logo-mobile" >
-				
+				<img class="logo-small" src="<?php bloginfo('template_url') ?>/images/logo-mobile.jpg" alt="logo-mobile" >
 			</a>
-		</div>
-		<div id="nav-icon">
+		</div><!-- .logo -->
+		
+		<div id="nav-icon" class="trigger-overlay">
 		  <span></span>
 		  <span></span>
 		  <span></span>
-		</div>
-	</div>
+		</div><!-- #nav-icon -->
+		
+	</div><!-- .nav-wrap -->
+	
 	<div class="header-wrap">
 		<div class="header">
 			<?php if (is_front_page() ){ ?>
 				<img src="<?php bloginfo('template_url') ?>/images/banner-home-inner.png" alt="banner-home-inner" width="411" height="252">
 				<div class="tagline">
 					Your Future Starts Here
-				</div>
+				</div><!-- .tagline -->
 			<?php	} else { ?>
 			
 			<div class="page_title">
 				<?php the_title(''); ?>
-			</div>
+			</div><!-- .page_title -->
 			<?php } ?>
-		</div>
-		<div class="trigger_nav"></div><!-- .trigger_nav -->
-	</div>	
+		</div><!-- .header -->
+	</div><!-- .header-wrap -->	
